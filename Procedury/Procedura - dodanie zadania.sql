ee42c2cdb6e593c5a6e94390c9d3379c44ec4f0b
AmikCS <139073745+AmikCS@users.noreply.github.com>
2024-08-08 22:48:40 +0200

    Update and rename Procedura - dodanie zadania.sql to Procedury/Procedura - dodanie zadania.sql

---
+++ Procedury/Procedura - dodanie zadania.sql
@@ -0,0 +1,19 @@
+﻿USE [Polcar_test]
+GO
+/****** Object:  StoredProcedure [dbo].[AddTask]    Script Date: 08.08.2024 20:33:31 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[AddTask]
+	@TenantID INT,
+	@CreatedByUserId INT,
+	@Title VARCHAR(200),
+	@Priority VARCHAR(50),
+	@Descriptions VARCHAR(MAX),
+	@STATUS VARCHAR(50)
+AS
+BEGIN
+	INSERT INTO Tasks (TenantID, CreatedByUserID, Title, Priority, Descriptions, Status)
+	VALUES(@TenantID, @CreatedByUserId, @Title, @Priority, @Descriptions, @STATUS);
+END;
